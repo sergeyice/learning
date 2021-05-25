fea2e3f66cc8e5b58d74e62d39cffc70edcb4bef
sergeyice <sergeyice72@gmail.com>
2021-05-26 00:35:01 +0300

    test http server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Привет Мир!');
+let http = require('http');
+
+let port = 80;
+
+let server = http.createServer((req, res) => {
+
+    console.log(req.url);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end(req.url === '/' ? 'Hello World!' : 'WAT?');
+
+});
+
+server.listen(port, () => {
+  console.log('Сервер работает по адресу: http://localhost:' + port);
+});
